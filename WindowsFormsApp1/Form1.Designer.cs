ab625d03b89b4db30e98e6921bc3b934ed15f0fc
dcoturel <dcoturel@gmail.com>
2019-02-07 20:11:13 -0300

    Commit inicial

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,70 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.cmdAccion = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(100, 38);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(256, 20);
+			this.txtNombre.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(28, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(44, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Nombre";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Location = new System.Drawing.Point(100, 150);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(0, 13);
+			this.lblResultado.TabIndex = 2;
+			// 
+			// cmdAccion
+			// 
+			this.cmdAccion.Location = new System.Drawing.Point(100, 81);
+			this.cmdAccion.Name = "cmdAccion";
+			this.cmdAccion.Size = new System.Drawing.Size(75, 23);
+			this.cmdAccion.TabIndex = 3;
+			this.cmdAccion.Text = "Ejecutar";
+			this.cmdAccion.UseVisualStyleBackColor = true;
+			this.cmdAccion.Click += new System.EventHandler(this.cmdAccion_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.cmdAccion);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtNombre);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblResultado;
+		private System.Windows.Forms.Button cmdAccion;
 	}
 }
 
